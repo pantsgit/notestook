c9785fba4205e3894faf66a1dcecb2b88cef3c98
pantsgit <danbaitzel@gmail.com>
2025-05-13 05:20:00 +0300

    lesson in comment

--- stuff.c
+++ stuff.c
@@ -8,5 +8,12 @@ void function(int nums[5]) {
 int main() { 
 	int nums[5] = {10, 20, 30, 40, 50};
 	function(nums);
+	
+	int new_nums [5] = {2, 4, 8, 10, 12};
+	function(new_nums); 
+
+	//allegedly, the function can work with any array (as long as it's the correct size), regardless of the fact that 
+	//it was initially defined with nums as opposed to new_nums. skeptical but let's see. 
+
 	return 0;
 }
